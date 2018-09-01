b725010c3313a079ea19b538634bbd07d17d3ac6
Artem Chursin <tema320d@gmail.com>
2018-09-01 18:27:29 +0300

    add fastline

--- a.chursin/App/Flows/UIControllers/AccauntUIControllers/AccauntDataViewController.swift
+++ a.chursin/App/Flows/UIControllers/AccauntUIControllers/AccauntDataViewController.swift
@@ -37,7 +37,6 @@ class AccauntDataViewController: UIViewController, TrackableMixin
         printUser()
     }
     
-
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
